a664375ccf03a6cc6a06feda07fe768af699713d
Tech City Ventures <abdi@techcity.ventures>
2020-10-25 14:57:15 +0000

    FE changes

--- HackathonTeam3/src/screens/HomeScreen.js
+++ HackathonTeam3/src/screens/HomeScreen.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, {useState, useEffect} from 'react';
 import {
   View,
@@ -8,32 +7,26 @@ import {
   Text,
   SafeAreaView,
   Image,
+  ScrollView,
 } from 'react-native';
 import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
 import {RFPercentage} from 'react-native-responsive-fontsize';
-=======
-
-import React, { useState, useEffect } from 'react';
-import { View, StyleSheet, FlatList, TouchableOpacity, Text, SafeAreaView, Image, ScrollView} from 'react-native';
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { RFPercentage } from 'react-native-responsive-fontsize';
->>>>>>> 69236ed8fc7e929ce649fbb663b4d3cd484e4f24
 
 import Flags from '../components/Flags';
 import NewsCard from '../components/NewsCard';
 import Margin from '../components/Margin';
 import NavHeader from '../components/NavHeader';
-<<<<<<< HEAD
-import {announcementsUrl} from '../constants';
+import DropDownPicker from 'react-native-dropdown-picker';
 import {GET} from '../lib/requests';
+import {announcementsUrl} from '../constants';
 
 const HomeScreen = ({navigation}) => {
   const [isRefreshing, setIsRefreshing] = useState(false);
   const [data, setData] = useState([]);
+  const [language, setLanguage] = useState('Somali');
 
   const getData = async () => {
-    console.log('hi');
-    let dog = await GET({url: announcementsUrl}, (err, response) => {
+    await GET({url: announcementsUrl}, (err, response) => {
       if (err) {
         throw err;
       }
@@ -46,7 +39,10 @@ const HomeScreen = ({navigation}) => {
   }, []);
 
   useEffect(() => {
-    getData();
+    setIsRefreshing(false);
+  }, [isRefreshing]);
+
+  useEffect(() => {
     setIsRefreshing(false);
   }, [isRefreshing]);
 
@@ -54,13 +50,19 @@ const HomeScreen = ({navigation}) => {
     <SafeAreaView style={styles.screen}>
       <View style={styles.headerView}>
         <Text style={styles.headerText}>Announcements</Text>
-        <TouchableOpacity onPress={() => navigation.navigate('Notification')}>
-          <FontAwesomeIcon color="black" icon="bell" size={RFPercentage(3.6)} />
-        </TouchableOpacity>
       </View>
       {/* <NavHeader backPress={()=> navigation.goBack()}/> */}
-      <Flags />
-      <View style={{padding: '2%', marginBottom: '10%'}}>
+      <DropDownPicker
+        items={[
+          {label: 'Somali', value: 'Somali'},
+          {label: 'English', value: 'English'},
+        ]}
+        defaultIndex={1}
+        defaultValue="Somali"
+        containerStyle={{height: 40}}
+        onChangeItem={(item) => setLanguage(item.value)}
+      />
+      <ScrollView style={{padding: '2%', marginBottom: '10%'}}>
         <FlatList
           data={data}
           keyExtractor={(item) => item.id}
@@ -69,11 +71,26 @@ const HomeScreen = ({navigation}) => {
           renderItem={({item}) => (
             <View style={styles.flatlistView}>
               <NewsCard
-                title={item?.story?.title}
-                body={item?.story?.description}
+                title={
+                  language === 'Somali'
+                    ? item?.somaliStory?.title
+                    : item?.story?.title
+                }
+                body={
+                  language === 'Somali'
+                    ? item?.somaliStory?.description
+                    : item?.story?.description
+                }
+                readMore={
+                  language === 'Somali' ? 'Akhri wax dheeraad ah' : 'Read More'
+                }
                 newsPress={() =>
                   navigation.navigate('News', {
                     news: item,
+                    newsCategory: 'GOV.UK',
+                    newsTitle: item.title,
+                    newsContent: item.body,
+                    newsDate: '4 days ago',
                   })
                 }
               />
@@ -81,160 +98,44 @@ const HomeScreen = ({navigation}) => {
             </View>
           )}
         />
-      </View>
+      </ScrollView>
     </SafeAreaView>
   );
-=======
-import DropDownPicker from 'react-native-dropdown-picker';
-
-
-const data = [
-    {
-        id: 1,
-        title: "Pivotal Studies of Covid-19 Vaccines From AstraZeneca, J&J Resuming - The Wall Street Journal",
-        body: "Paused studies of leading Covid-19 vaccine candidates from AstraZeneca PLC and Johnson "
-
-    },
-    {
-        id: 2,
-        title: "Azerbaijani president lays out conditions for Armenia cease-fire - Fox News",
-        body: "LONDON As fighting rages between Azerbaijan and Armenia over the disputed breakaway "
-
-    },
-    {
-        id: 3,
-        title: "With Debates Behind Them, Biden And Trump Enter The Final Stretch Before Election Day - NPR",
-        body: "Former Vice President Joe Biden focuses on the coronavirus in an address Friday in Wilmington",
-
-    },
-    {
-        id: 4,
-        title: "Early iPhone 12 Tests Show Ceramic Shield is Stronger and More Scratch Resistant Than iPhone 11 Glass - MacRumors",
-        body: "Apple's new iPhone 12 models are protected by a Ceramic Shield cover glass that has nano-ceramic crystals infused right "
-
-    },
-]
-
-const HomeScreen = ({ navigation }) => {
-
-    const [isRefreshing, setIsRefreshing] = useState(false)
-
-    useEffect(() => {
-        setIsRefreshing(false)
-    }, [isRefreshing]);
-
-    return (
-        <SafeAreaView style={styles.screen}>
-            <View style={styles.headerView}>
-                <Text style={styles.headerText}>Announcements</Text>
-
-            </View>
-            {/* <NavHeader backPress={()=> navigation.goBack()}/> */}
-            <DropDownPicker
-                items={[
-                    {label: 'English', value: 'item1'},
-                    {label: 'Somali', value: 'item2'},
-                ]}
-                defaultIndex={1}
-                defaultValue="item1"
-                containerStyle={{height: 40}}
-                onChangeItem={item => console.log(item.label, item.value)}
-            />
-            <ScrollView style={{ padding: '2%', marginBottom: '10%' }}>
-                <FlatList
-                    data={data}
-                    keyExtractor={item => item.id}
-                    refreshing={isRefreshing}
-                    onRefresh={() => setIsRefreshing(true)}
-                    renderItem={({ item }) => (
-                        <View style={styles.flatlistView}>
-                            <NewsCard
-                              title={item.title}
-                              body={item.body}
-                              newsPress={() => navigation.navigate('News', {
-                                  news: item,
-                                  newsCategory:'GOV.UK',
-                                  newsTitle:item.title,
-                                  newsContent:item.body,
-                                  newsDate:'4 days ago',
-                              })}
-                        />
-                            <Margin size={2} />
-                        </View>
-
-                    )}
-                />
-            </ScrollView>
-
-        </SafeAreaView>
-
-    );
-
->>>>>>> 69236ed8fc7e929ce649fbb663b4d3cd484e4f24
 };
 
 // onPress={() => navigation.navigate('Notification')
 export const screenOptions = {
-<<<<<<< HEAD
-  headerShown: false,
-=======
-
   headerStyle: {
-      backgroundColor: '#FDD670',
-      shadowColor: 'transparent'
-    },
+    backgroundColor: '#FDD670',
+    shadowColor: 'transparent',
+  },
   title: '',
   headerShown: true,
-  headerRight:   () => (
-    <TouchableOpacity >
-        <FontAwesomeIcon color="" icon="bell" size={RFPercentage(3)} />
+  headerRight: () => (
+    <TouchableOpacity>
+      <FontAwesomeIcon color="" icon="bell" size={RFPercentage(3)} />
     </TouchableOpacity>
   ),
-
->>>>>>> 69236ed8fc7e929ce649fbb663b4d3cd484e4f24
 };
 
-
-
 const styles = StyleSheet.create({
-<<<<<<< HEAD
   screen: {
     flex: 1,
+    backgroundColor: 'white',
   },
   headerView: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
     paddingHorizontal: '2%',
+    paddingVertical: '5%',
   },
   headerText: {
-    fontSize: RFPercentage(4),
-    fontWeight: 'bold',
-    fontFamily: 'Inter-Bold',
+    fontSize: RFPercentage(5.5),
+    alignItems: 'center',
+    fontFamily: 'DMSerifDisplay-Regular',
   },
   flatlistView: {},
-=======
-    screen: {
-        flex: 1,
-        backgroundColor:'white'
-    },
-    headerView: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        paddingHorizontal: '2%',
-        paddingVertical: '5%'
-
-    },
-    headerText: {
-        fontSize: RFPercentage(5.5),
-        alignItems: 'center',
-        fontFamily: 'DMSerifDisplay-Regular',
-
-    },
-    flatlistView: {
-    }
->>>>>>> 69236ed8fc7e929ce649fbb663b4d3cd484e4f24
 });
 
 export default HomeScreen;
--- HackathonTeam3/src/screens/NewsScreen.js
+++ HackathonTeam3/src/screens/NewsScreen.js
@@ -1,98 +1,103 @@
-import React from 'react';
-<<<<<<< HEAD
-import {View, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {
+  View,
+  ScrollView,
+  StyleSheet,
+  Dimensions,
+  TouchableOpacity,
+  Text,
+} from 'react-native';
+import TextComponent from '../components/TextComponent';
+import DropDownPicker from 'react-native-dropdown-picker';
+import HTML from 'react-native-render-html';
 
-const NewsScreen = () => {
-  return <View style={styles.screen} />;
+const NewsScreen = ({route, navigation}) => {
+  const {newsTitle, newsCategory, newsContent, news} = route.params;
+  const [language, setLanguage] = useState('Somali');
+
+  return (
+    <View style={styles.screen}>
+      <View style={{flex: 1, backgroundColor: '#FDD670'}} />
+      <DropDownPicker
+        items={[
+          {label: 'Somali', value: 'Somali'},
+          {label: 'English', value: 'English'},
+        ]}
+        defaultIndex={1}
+        defaultValue="Somali"
+        containerStyle={{height: 40}}
+        onChangeItem={(item) => setLanguage(item.value)}
+      />
+      <View style={{flex: 3, paddingHorizontal: '6%'}}>
+        <TextComponent
+          title={
+            language === 'Somali'
+              ? news?.somaliStory?.title
+              : news?.story?.title
+          }
+          colour={'black'}
+          size={30}
+          align={'center'}
+          weight={'bold'}
+        />
+        <View
+          style={{
+            flexDirection: 'row',
+            paddingVertical: '6%',
+            textAlign: 'center',
+          }}>
+          <TextComponent
+            title={newsCategory}
+            colour={'grey'}
+            size={15}
+            align={'left'}
+            flex={1}
+          />
+          <TextComponent
+            title={'2 days ago'}
+            colour={'grey'}
+            size={15}
+            flex={1}
+            align={'right'}
+          />
+        </View>
+        <ScrollView>
+          <HTML
+            html={
+              language === 'Somali'
+                ? news?.somaliStory?.body
+                : news?.story?.body
+            }
+            imagesMaxWidth={Dimensions.get('window').width}
+          />
+
+          {/* <TextComponent
+            title={}
+            colour={'black'}
+            size={15}
+            flex={4}
+            align={'justify'}
+          /> */}
+        </ScrollView>
+      </View>
+    </View>
+  );
 };
 
 export const screenOptions = {
-  headerTitle: 'Detail',
+  headerStyle: {
+    backgroundColor: '#FDD670',
+    shadowColor: 'transparent',
+  },
+  title: '',
+  headerShown: true,
 };
 
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
+    backgroundColor: 'white',
   },
-=======
-import { View, ScrollView, StyleSheet, TouchableOpacity, Text} from 'react-native';
-import TextComponent from '../components/TextComponent';
-import DropDownPicker from 'react-native-dropdown-picker';
-const NewsScreen = ({route, navigation}) => {
-const { newsTitle , newsCategory, newsContent, news} = route.params;
-
-
-    return (
-        <View style={styles.screen}>
-        <View style={{flex: 1,backgroundColor: '#FDD670'}} />
-        <DropDownPicker
-            items={[
-                {label: 'English', value: 'item1'},
-                {label: 'Somali', value: 'item2'},
-            ]}
-            defaultIndex={1}
-            defaultValue="item1"
-            containerStyle={{height: 40}}
-            onChangeItem={item => console.log(item.label, item.value)}
-        />
-        <View style={{flex: 3, paddingHorizontal: '6%', }}>
-
-          <TextComponent
-              title={newsTitle}
-              colour={'black'}
-              size={30}
-              align={'center'}
-              weight={'bold'}
-          />
-          <View style={{flexDirection:'row', paddingVertical: '6%', textAlign:'center'}}>
-            <TextComponent
-                title={newsCategory}
-                colour={'grey'}
-                size={15}
-                align={'left'}
-                flex={1}
-            />
-            <TextComponent
-                title={'2 days ago'}
-                colour={'grey'}
-                size={15}
-                flex={1}
-                align={'right'}
-            />
-          </View>
-          <ScrollView>
-          <TextComponent
-              title={'It continues to be the aim that all pupils, in all year groups, remain in school full-time throughout the autumn term.This guidance is intended to support schools, both mainstream and alternative provision, to prepare for this. It applies to primary, secondary (including sixth forms), post-16 academies, infant, junior, middle, upper, school-based nurseries and boarding schools. We expect independent schools to follow the control measures set out in this document in the same way as state-funded schools. The guidance also covers expectations for children with special educational needs and disability (SEND), including those with education, health and care plans, in mainstream schools.'}
-              colour={'black'}
-              size={15}
-              flex={4}
-              align={'justify'}
-          />
-          </ScrollView>
-        </View>
-
-        </View>
-    );
-
-};
-
-export const screenOptions = {
-
-  headerStyle: {
-      backgroundColor: '#FDD670',
-      shadowColor: 'transparent'
-    },
-  title: '',
-  headerShown: true,
-
-};
-
-const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        backgroundColor: 'white'
-    }
->>>>>>> 69236ed8fc7e929ce649fbb663b4d3cd484e4f24
 });
 
 export default NewsScreen;
